f0d0b33b0bc68e3efe927a7b533dddf8160e0958
Ze0wkiii <iamze0wk@gmail.com>
2025-10-26 17:58:59 +0530

    Program Complete
    Successfully added all the basic stuff in the program. Fine touch and all is done.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 from datetime import datetime
 import csv
 from data_entry import get_date, get_amount, get_category, get_description
+from time import sleep
 
 
 dashboard = {
@@ -38,28 +39,37 @@ class CSV:
     def get_transactions(cls, start_date, end_date):
         df = pd.read_csv(cls.CSV_file)
         df['date'] = pd.to_datetime(df["date"], format=CSV.FORMAT)
-        start_date = datetime.strptime(start_date, format=CSV.FORMAT)
-        end_date = datetime.strptime(end_date, format=CSV.FORMAT)
-        mask = (df["date"]>=start_date & df['date']<=end_date)
+        start_date = datetime.strptime(start_date, CSV.FORMAT)
+        end_date = datetime.strptime(end_date, CSV.FORMAT)
+        mask = (df["date"] >= start_date) & (df["date"] <= end_date)
+
         filtered_df = df.loc[mask]
         if filtered_df.empty:
             print("No transacations found in the given data range.")
         else:
             print(f"Transacations from {start_date.strftime(CSV.FORMAT)} to {end_date.strftime(CSV.FORMAT)}")
+            sleep(1)
 
             print(filtered_df.to_string(index=False, formatters={"date": lambda x: x.strftime(CSV.FORMAT)}))
+            sleep(1)
 
             total_income = filtered_df[filtered_df['category']== 'Income']['amount'].sum()
             total_expense = filtered_df[filtered_df['category']== 'Expense']['amount'].sum()
             print("\nSummary: ")
+            sleep(2)
             print(f"Total Income:{total_income:.2f}")
+            sleep(1)
             print(f"Total Expense:{total_expense:2f}")
-            print(f"Total Savings:{total_income - total_expense}):.2f")
+            sleep(1)
+            print(f"Total Savings:{total_income - total_expense:.2f}")
 
         
-    @classmethod
-    def transactions_get(cls):
-        pass
+    # @classmethod
+    # def transactions_get(cls):
+    #     start_date = "Please enter the start-date (dd-mm-yyy): "
+    #     end_date = "Please enter the end-date (dd-mm-yyy): "
+    #     CSV.get_transactions(start_date, end_date)
+
 
 
 
@@ -71,27 +81,36 @@ def add():
     description = get_description()
     CSV.add_entry(date,amount,category,description)
 
-add()
 
 def main():
-    print("--Welcome to your Personal Finance Tracker--")
-    for key, value in dashboard.items():
-        return print(f"{key}:{value}")
-    choice = int(input("Enter your Choice: "))
-    try:
-        if choice in dashboard and choice==1:
-            add()
-        elif choice in dashboard and choice==2:
-            CSV.transactions_get()
-        elif choice in dashboard and choice==3:
-            return exit
-        else:
-            raise ValueError("The Value you have entered is incorrect (type either of them 1, 2 or 3 for desired function)")
-    except ValueError as e:
-        print(e)
-        return main()
-        
+    exit_program=0
+    while exit_program==False:
+        print("--Welcome to your Personal Finance Tracker--")
 
+        for keys, values in dashboard.items():
+            print(f"{keys}:{values}")
+        choice = int(input("Enter your Choice: "))
+
+        try:
+            if choice in dashboard and choice==1:
+                add()
+
+            elif choice in dashboard and choice==2:
+                start_date = input("Please enter the start-date (dd-mm-yyy): ")
+                end_date = input("Please enter the end-date (dd-mm-yyy): ")
+                CSV.get_transactions(start_date, end_date)
+
+            elif choice in dashboard and choice==3:
+                exit_program=True
+                print("Exiting the program")
+                exit()
+            else:
+                raise ValueError("The Value you have entered is incorrect (type either of them 1, 2 or 3 fordesired function)")
+        
+        except ValueError as e:
+            print(e)
+        
+main()
         
 
 
